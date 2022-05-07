b6e378e79315895afd087624cca9a364512af64a
RustamIR <66008972+RustamIR@users.noreply.github.com>
2022-05-07 10:58:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-API_TOKEN = '5101940487:AAFRGeg3q4LM8z9lLvRJIcw81eHPYAHFOU4'
+API_TOKEN = ''
 
 storage = MemoryStorage()
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
 ADMINS = [
-    291800214
+
 ] # посмотреть свой Id в get bot id
